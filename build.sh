620f6db141f92aa63e5b10dc27d1bfe9c02a63ce
xawlw <abdulazizawlw@gmail.com>
2020-04-09 14:12:23 +0300

    drop O=out

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 export ARCH=arm
 export SUBARCH=arm
-make O=out ARCH=arm lineageos_bacon_defconfig
+make ARCH=arm lineageos_bacon_defconfig
 
 #PATH="${PWD}/bin:${PWD}/toolchain/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      CC=/home/xawlw/kernel/lolz/bin/clang \
+make -j$(nproc --all) CC=/home/xawlw/kernel/lolz/bin/clang \
                       HOSTCC=/home/xawlw/kernel/lolz/bin/clang \
                       CROSS_COMPILE=/home/xawlw/xtnd/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-linaro-7/bin/arm-linux-gnueabi-
