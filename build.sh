3aed38642e5ace3e34f7113cd768aaef7f8a5137
xawlw <abdulazizawlw@gmail.com>
2020-04-12 15:42:18 +0300

    Customize build script

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-export CCACHE_DISABLE=1
 export ARCH=arm
 export SUBARCH=arm
-export LD_LIBRARY_PATH="/run/media/tfonda/HDD/android/lolz_clang/lib"
+CLANG=/home/xawlw/kernel/lolz/bin/clang
+GCC=/home/xawlw/xtnd/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-linaro-7/bin/arm-linux-gnueabi-
+make lineageos_bacon_defconfig \
+	 CC=$CLANG \
+	 HOSTCC=$CLANG \
+	 CROSS_COMPILE=$GCC
 
-make lineageos_bacon_defconfig CC=/run/media/tfonda/HDD/android/lolz_clang/bin/clang HOSTCC=/run/media/tfonda/HDD/android/lolz_clang/bin/clang CROSS_COMPILE=/run/media/tfonda/HDD/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
-
-make -j5 CC=/run/media/tfonda/HDD/android/lolz_clang/bin/clang HOSTCC=/run/media/tfonda/HDD/android/lolz_clang/bin/clang CROSS_COMPILE=/run/media/tfonda/HDD/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
-
-../dtbToolCM -2 -o ../AnyKernel3/dt -s 2048 -p scripts/dtc/ arch/arm/boot/
-
-cp arch/arm/boot/zImage ../AnyKernel3/
-cd ../AnyKernel3
-zip -r ../$1 anykernel.sh dt META-INF/ tools/ zImage
+make -j$(nproc --all) CC=$CLANG \
+                      HOSTCC=$CLANG \
+                      CROSS_COMPILE=$GCC
